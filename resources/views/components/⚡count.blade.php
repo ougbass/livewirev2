9d66c7b9553633278a17dba9f436b44f77c3cde4
ougbass <gsbassanezi@gmail.com>
2026-04-19 23:52:08 -0300

    toggle fn

--- resources/views/components/⚡count.blade.php
+++ resources/views/components/⚡count.blade.php
@@ -9,14 +9,21 @@ new class extends Component
     // protected and private properties don't show on our view
     protected string $email = 'email@email.com';
     private ?string $number = '+55555555555';
+
+    public function toggle()
+    {
+        $this->name = str($this->name)->upper();
+    }
 };
 ?>
 
 <div>
-    // This way the $name property waits an action
-    <x-text-input wire:model="name" type="text"/> {{ $name }}
+     {{-- This way the $name property waits an action --}}
+    <x-text-input wire:model="name" type="text"/>
+    <x-primary-button wire:click='toggle'>Lower</x-primary-button>
+    <x-secondary-button wire:click='toggle'>Upper</x-secondary-button>
 
-    //This way the $name property updates as we type
-    //new in Livewire 3, no need to add .live modifier
-    <x-text-input wire:model.live="name"/> {{ $name }}
+    {{-- //This way the $name property updates as we type
+    //new in Livewire 3, no need to add .live modifier --}}
+    {{-- <x-text-input wire:model.live="name"/> {{ $name }} --}}
 </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,12 +10,8 @@
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
-                    <livewire:count />
-                    <livewire:count />
-                    <livewire:count />
-                    <livewire:count />
-                    <livewire:count />
                 </div>
+                <livewire:count />
 
             </div>
         </div>
