a363300ba0a9e8a7c416d12415e0be2c2384e01a
ougbass <gsbassanezi@gmail.com>
2026-05-03 13:22:17 -0300

    object real time validation

--- resources/views/components/⚡hello.blade.php
+++ resources/views/components/⚡hello.blade.php
@@ -8,6 +8,10 @@ new class extends Component
     public string $text = '';
     public ?User $user = null;
 
+    protected array $rules = [
+        'text' => ['required', 'min:4']
+    ];
+
     public function boot()
     {
         ds(__METHOD__ );
@@ -34,19 +38,26 @@ new class extends Component
         ds(__METHOD__);
     }
 
-    public function updating($a, $b)
+    public function updating()
     {
-        ds(__METHOD__, compact('a', 'b'));
+
+        ds(__METHOD__);
     }
 
-    public function updated()
+    public function updated($prop, $value)
     {
-        ds(__METHOD__);
+        $this->validateOnly($prop);
+
+        ds(__METHOD__, compact('prop', 'value'));
     }
 };
 ?>
 
 <div>
     <x-text-input wire:model.live='text'></x-text-input> <br>
+    @error('text')
+        <span class="alert alert-danger">{{ $message }}</span>
+    @enderror
     {{ $user->name }}
+
 </div>
