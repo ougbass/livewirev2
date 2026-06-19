a0e4be6b6f10867b5b58411470b9d8caecacd19b
ougbass <gsbassanezi@gmail.com>
2026-06-18 21:44:24 -0300

    Users list component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,6 +17,7 @@
 
             <div class="mb-8 overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{-- Users List Component --}}
                     <livewire:users-list />
                 </div>
             </div>
